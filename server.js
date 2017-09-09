ad1b030ae2e48609510c298f58711905647cee33
Yajan <31155354+Yajan@users.noreply.github.com>
2017-09-09 18:39:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one',function (req,res){
  res.send('article one is requested');   
 });
 
-app.get('/article-one',function (req,res){
+app.get('/article-two',function (req,res){
  res.send('article two is requested');   
 });
 
-app.get('/article-one',function (req,res){
+app.get('/article-three',function (req,res){
  res.send('article three is requested');   
 });
 
