0ee42ea92ca43b7f3d6486da3798a53bd74de0fe
Yajan <31155354+Yajan@users.noreply.github.com>
2017-09-09 18:38:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one',function (req,res){
  res.send('article one is requested');   
 });
 
+app.get('/article-one',function (req,res){
+ res.send('article two is requested');   
+});
+
+app.get('/article-one',function (req,res){
+ res.send('article three is requested');   
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
